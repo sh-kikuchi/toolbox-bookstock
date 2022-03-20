52ee1e44e8f5a62566b1d4d68671de2ffaa90ca6
sh-kikuchi <shu94hist726@gmail.com>
2022-03-20 20:24:00 +0900

    md_and_favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,9 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
+     <!-- favicon-->
+     <link rel="shortcut icon" type="image/x-icon"  href="{{ asset('/public/images/favicon.ico') }}">
+
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
